7ced7e6f01300012ac8463a89daef7e6b3e457f3
Will Hopkins <willothyh@gmail.com>
2022-05-19 23:29:12 -0700

    PyO3 example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use pyo3::prelude::*;
+use pyo3::types::IntoPyDict;
+
+fn main() -> PyResult<()> {
+    pyo3::prepare_freethreaded_python();
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let sys = py.import("sys")?;
+    let version = sys.getattr("version")?;
+    let locals = [("os", py.import("os")?)].into_py_dict(py);
+    let user = py.eval("os.getenv('USER') or os.getenv('USERNAME')", None, Some(&locals)).unwrap();
+
+    println!("Hello {}, I'm python {}", user, version);
+    Ok(())
 }
